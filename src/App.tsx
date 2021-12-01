31ecca999d7f413917523abe104dbf40467ecec6
Lucas Almeida <39913588+lucas-almeida026@users.noreply.github.com>
2021-12-01 18:47:50 -0300

    theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import { ThemeProvider } from '@mui/material'
 import React from 'react'
 import Routes from './routes'
+import theme from './theme'
 
 function App() {
   return (
-    <Routes />
+    <ThemeProvider theme={theme}>
+      <Routes />
+    </ThemeProvider>
   )
 }
 
